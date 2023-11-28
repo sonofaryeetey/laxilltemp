3ec2ec6631c091b2dcaee95733e1773afbc4e2c8
joshua <ayiniiayi@gmail.com>
2023-11-28 15:38:18 +0000

    fixed apostrophe issue

--- src/app/components/AboutUs.jsx
+++ src/app/components/AboutUs.jsx
@@ -12,7 +12,7 @@ const AboutUs = ({Data}) => {
             <div class="one gradient"></div>
             <div class="two gradient"></div>
         </div>
-        <p className=''>Our team of experienced data scientists, analysts, and engineers are committed to delivering innovative solutions that empower your organization. With a track record of success, we've helped numerous companies across various industries leverage their data for competitive advantage.
+        <p className=''>{`Our team of experienced data scientists, analysts, and engineers are committed to delivering innovative solutions that empower your organization. With a track record of success, we've helped numerous companies across various industries leverage their data for competitive advantage.`}
 </p>
         <button className='px-11 text-white font-semi-bold py-3 rounded-full mr-4 mt-10 gradient-button '>See more</button>
         </div>
